e7f4f83e93089051f8573b29c7f7a1125816a929
Josh Duff <me@JoshDuff.com>
2016-05-23 20:34:07 -0500

    Faster loading: all tests passing but one

--- index.js
+++ index.js
@@ -12,6 +12,21 @@ var BASE_DATA = {
 	posts: {}
 }
 
+function postsToPostList(posts) {
+	return posts.reverse().filter(function(post) {
+		return typeof post.metadata.title === 'string' && post.metadata.date
+	}).map(function(post) {
+		return extend(post, post.metadata)
+	})
+}
+
+function postsToPostObject(posts) {
+	return posts.reduce(function(posts, post) {
+		posts[removeDots(post.filename)] = post
+		return posts
+	}, {})
+}
+
 module.exports = function renderDom(rootPostOrString, options, cb) {
 	if (!options || !options.linkifier || !options.butler) {
 		throw new Error('Expected linkifier and butler properties on options object.')
@@ -19,8 +34,11 @@ module.exports = function renderDom(rootPostOrString, options, cb) {
 	var butler = options.butler
 	cb = oneTime(cb)
 
-	postOrString(rootPostOrString, butler, function (err, rootPost) {
+	postOrString(rootPostOrString, butler, function(err, rootPost) {
 		if (err) return cb(err)
+		var postList = []
+		var posts = {}
+
 		var state = {
 			filenameUuidsMap: {},
 			uuidArgumentsMap: {}
@@ -34,6 +52,25 @@ module.exports = function renderDom(rootPostOrString, options, cb) {
 			// staticTripleDelimiters: [ '[[[static]]]', '[[[/static]]]' ],
 			template: makePartialString(rootPost.filename)
 		})
+
+		var updatePosts = onlyOneAtATime(function updatePosts(done) {
+			butler.getPosts(function(err, postsArray) {
+				if (err) return console.error(err)
+
+				postList = postsToPostList(postsArray)
+				posts = postsToPostObject(postsArray)
+
+				ractive.set({
+					postList: postList,
+					posts: posts
+				})
+
+				done()
+			})
+		})
+
+		updatePosts()
+
 		function resetPartial(partialName, templateString) {
 			ractive.resetPartial(partialName, '[[=[[static]] [[/static]]=]]\n' + templateString) // See issue #18
 		}
@@ -52,31 +89,22 @@ module.exports = function renderDom(rootPostOrString, options, cb) {
 			postOrString(currentPostOrString, butler, function (err, currPost) {
 				if (err) return onLoadCb(err)
 
-				augmentCurrentData(currPost, butler, { local: true }, function(err, postListData) {
-					if (err) return onLoadCb(err)
-
-					scan(currPost, util, state, currentFilename === currPost.filename, function() {
-						var startingData = extend(BASE_DATA, options.data || {}, postListData, setCurrentData, {
-							removeDots: removeDots,
-							metadata: currPost.metadata,
-							current: currPost.filename
-						})
-
-						currentFilename = currPost.filename
-
-						ractive.reset(startingData) // reset() removes old data
-
-						onLoadCb(null)
-
-						augmentCurrentData(currPost, butler, { local: false }, function(err, data) {
-							if (err) {
-								console.error(err)
-							}
-
-							ractive.set(data)
-						})
+				scan(currPost, util, state, currentFilename === currPost.filename, function() {
+					var startingData = extend(BASE_DATA, options.data || {}, setCurrentData, currPost.metadata, {
+						removeDots: removeDots,
+						metadata: currPost.metadata,
+						current: currPost.filename,
+						postList: postList,
+						posts: posts
 					})
+
+					currentFilename = currPost.filename
+
+					ractive.reset(startingData) // reset() removes old data
+
+					onLoadCb(null)
 				})
+
 			})
 		}
 
@@ -91,7 +119,7 @@ module.exports = function renderDom(rootPostOrString, options, cb) {
 			resetPartial: resetPartial
 		}
 
-		butler.on('post changed', function (filename, post) {
+		butler.on('post changed', function(filename, post) {
 			if (partialExists(filename)) { // Only cares about posts that are in the system
 				if (filename === currentFilename) { // Current post changed
 					setCurrent(post)
@@ -100,6 +128,8 @@ module.exports = function renderDom(rootPostOrString, options, cb) {
 				}
 			}
 		})
+
+		butler.on('index changed', updatePosts)
 
 		scan(rootPost, util, state)
 		cb(null, setCurrent)
@@ -195,26 +225,19 @@ function postOrString(post, butler, cb) {
 	}
 }
 
-function augmentCurrentData(post, butler, options, cb) {
-	butler.getPosts(options, function(err, posts) {
-		if (err) {
-			cb(err)
-		} else {
-			cb(null, extend(post.metadata, {
-				postList: posts.reverse().filter(function(post) {
-					return typeof post.metadata.title === 'string' && post.metadata.date
-				}).map(function(post) {
-					return extend(post, post.metadata)
-				}),
-				posts: posts.reduce(function(posts, post) {
-					posts[removeDots(post.filename)] = post
-					return posts
-				}, {})
-			}))
-		}
-	})
-}
-
 function removeDots(str) {
 	return str.replace(/\./g, '')
+}
+
+function onlyOneAtATime(fn) {
+	var running = false
+
+	return function() {
+		if (!running) {
+			running = true
+			fn(function() {
+				running = false
+			})
+		}
+	}
 }
